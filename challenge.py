6591fb97f9ab1b82e100b7036bfb2b6f4f3d108d
ariarun <64533642+ariarun@users.noreply.github.com>
2024-04-16 23:47:42 -0700

    Finished input2
    Also find no county bug in input1 xml file, fixed

--- challenge.py
+++ challenge.py
@@ -1,5 +1,6 @@
 import xml.etree.cElementTree as ET
 import json
+import csv
 
 
 def read_xml():
@@ -30,8 +31,8 @@ def read_xml():
         temp['street'] = street
         temp['city'] = city
         temp['state'] = state
-        if country != " ":
-            temp['country'] = country
+        # No county stored in xml
+
 
         # To let zip in the format 00000 or 00000-0000
         print(postal_code, len(postal_code))
@@ -43,19 +44,86 @@ def read_xml():
 
         # Create a temp value to store the zip for sorting
         if "-" in temp['zip']:
-            temp["real_zip"] = postal_code[:5]+postal_code[-4:]
+            temp["fake_zip"] = postal_code[:5]+postal_code[-4:]
         else:
-            temp["real_zip"] = postal_code+"0000"
+            temp["fake_zip"] = postal_code+"0000"
 
         res.append(temp) # Add the dictionary to the result array
-    res.sort(key=lambda x: x['real_zip'])
+    res.sort(key=lambda x: x['fake_zip'])
     
     for i in res:
-        del i['real_zip']
+        del i['fake_zip']
 
     with open('output1.json', 'w') as json_file:
         json.dump(res, json_file, indent=4)
+    json_file.close()
 
+
+def read_tsv():
+    with open('input/input2.tsv', 'r', encoding='utf-8') as file:
+        reader = csv.DictReader(file, delimiter='\t')
+
+        res = []
+        for row in reader:
+            temp = {}
+            
+            # To create the whole name
+            first = row['first'].strip()
+            middle = row['middle'].strip() 
+            last = row['last'].strip()
+            name = ""
+            if first:
+                name += first + ' '
+            if middle and middle != "N/M/N": # N/M/N = no middle name
+                name += middle +' '
+            if last:
+                name += last +' '
+            name = name[:-1]
+
+            organization = row['organization'].strip() 
+
+
+            # Store basic info for address
+            street = row['address'].strip()
+            city = row['city'].strip()
+            state = row['state'].strip()
+            county = row['county'].strip()
+
+            if name:
+                temp['name'] = name
+            
+            elif organization:
+                temp['organization'] = organization
+
+            temp['street'] = street
+            temp['city'] = city
+            if county:
+                temp['county'] = county
+            temp['state'] = state
+
+
+            # Get the zip 
+            zip_code = row['zip'].strip()
+            zip4 = row['zip4'].strip()
+            if zip4:
+                temp['zip'] = zip_code+"-"+zip4
+                temp['fake_zip'] = zip_code+zip4
+            else:
+                temp['zip'] = zip_code
+                temp['fake_zip'] = zip_code+'0000'
+
+            res.append(temp)
+
+        res.sort(key=lambda x: x['fake_zip'])
+        
+        for i in res:
+            del i['fake_zip']
+
+        with open('output2.json', 'w') as json_file:
+            json.dump(res, json_file, indent=4)   
+
+        file.close()
+        json_file.close()
 
 
 
