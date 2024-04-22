d7dd75258a2bdf5441487c09f04eda3647f8d932
Omri Nuri <omrinuri@gmail.com>
2024-04-22 10:33:18 -0500

    feat: simple xml parsing, no validation yet

--- challenge.py
+++ challenge.py
@@ -1,5 +1,8 @@
 import argparse
 import os
+import xml.etree.ElementTree as ET
+import sys
+import json
 
 
 class FileTypeChecker(argparse.Action):
@@ -13,6 +16,54 @@ class FileTypeChecker(argparse.Action):
         setattr(namespace, self.dest, values)
 
 
+def parse_xml(file_path):
+    try:
+        tree = ET.parse(file_path)
+        root = tree.getroot()
+        entities = []
+
+        def extract_entity_data(entity, fields):
+            entity_data = {}
+            for field in fields:
+                element = entity.find(field)
+                assigned_field = field.lower()
+                value = (
+                    element.text.strip()
+                    if element is not None and element.text is not None
+                    else None
+                )
+                if assigned_field == "postal_code":
+                    value = value.replace(" ", "")
+                    if value.endswith("-"):
+                        value = value[:-1]
+                if value != "" and value is not None:
+                    entity_data[assigned_field] = value
+            return entity_data
+
+        fields = ["NAME", "COMPANY", "STREET", "CITY", "STATE", "POSTAL_CODE"]
+
+        for entity in root.findall(".//ENT"):
+            entity_data = extract_entity_data(entity, fields)
+            # remove all empty fields
+            entity_data = {k: v for k, v in entity_data.items() if v is not None}
+            entities.append(entity_data)
+
+        return entities
+    except ET.ParseError as e:
+        print(f"Error parsing XML file: {e}", file=sys.stderr)
+        sys.exit(1)
+
+
+def parse_txt(file_path):
+    # print("Parsing TXT file:", file_path)
+    return []
+
+
+def parse_tsv(file_path):
+    # print("Parsing TSV file:", file_path)
+    return []
+
+
 def main():
     parser = argparse.ArgumentParser(description="BW Challenge")
     parser.add_argument(
@@ -22,12 +73,22 @@ def main():
         help="List of file paths (txt, xml, tsv)",
     )
     args = parser.parse_args()
-
-    for file in args.files:
-        print(file)
-
-    # Use the validated file paths
     print("Validated file paths:", args.files)
+
+    all_entities = []
+    for file in args.files:
+        _, ext = os.path.splitext(file)
+        if ext == ".xml":
+            entities = parse_xml(file)
+        elif ext == ".txt":
+            entities = parse_txt(file)
+        elif ext == ".tsv":
+            entities = parse_tsv(file)
+        all_entities.extend(entities)
+
+    json_output = json.dumps(all_entities, indent=4)
+    print(json_output)
+    sys.exit(0)
 
 
 if __name__ == "__main__":
