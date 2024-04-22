8250f0537b100ea0903e14291179a4515c3f7846
kuraian <ryan.m.diep@gmail.com>
2024-04-21 19:26:28 -0700

    works

--- README.md
+++ README.md
@@ -11,9 +11,9 @@ and who are able to write clean, correct, and reliable code.
 The directory [`input`](input) contains three files each containing a list of US
 names and addresses:
 
-* [`input1.xml`](input/input1.xml)
-* [`input2.tsv`](input/input2.tsv)
-* [`input3.txt`](input/input3.txt)
+- [`input1.xml`](input/input1.xml)
+- [`input2.tsv`](input/input2.tsv)
+- [`input3.txt`](input/input3.txt)
 
 The file formats are not documented, but you can deduce the formats by examing
 their contents. The challenge is to write a python script `challenge.py`,
@@ -21,17 +21,17 @@ desgined to be run from the command line, that accepts a list of pathnames of
 files in any of the above formats, parses them, and writes a JSON-encoded list
 of the combined addresses to standard output, sorted by ZIP code in ascending order. You can assume
 that the the format of each file corresponds to its extension, as illustrated by
-the above examples. Your submission should consist of a single file, without any 
+the above examples. Your submission should consist of a single file, without any
 supporting documents. The output should be a pretty-printed JSON array of JSON
 objects, each having 5 or 6 properties, serialized in the given order:
 
-* `name`: The person's full name, if present, consisting of a list of one or more given names followed by the family name
-* `organization`: The company or organization name, if present
-* `street`: The street address, often just a house number followed by a direction indicator and a street name
-* `city`: The city name
-* `county:` The county name, if present
-* `state`: The US state name or abbreviation
-* `zip`: The ZIP code or ZIP+4, in the format 00000 or 00000-0000
+- `name`: The person's full name, if present, consisting of a list of one or more given names followed by the family name
+- `organization`: The company or organization name, if present
+- `street`: The street address, often just a house number followed by a direction indicator and a street name
+- `city`: The city name
+- `county:` The county name, if present
+- `state`: The US state name or abbreviation
+- `zip`: The ZIP code or ZIP+4, in the format 00000 or 00000-0000
 
 A personal name or organization name will always be present, but not both.
 
@@ -59,16 +59,16 @@ Here is a sample output:
 
 The script should
 
-* Be well-organized and easy to understand
-* Use only standard Python libraries
-* Be compatible with Python 3.11
-* Conform to [PEP 8](https://peps.python.org/pep-0008/)
-* Provide a `--help` option
-* Check for errors in the argument list
-* Check the input files to make sure they conform to the formats expemplified by the sample files 
-* Output a list of addresses only if no errors were discovered in the above two steps
-* Write any error messages to stderr
-* Exit with status `0` or `1` to indicate success or failure
+- Be well-organized and easy to understand
+- Use only standard Python libraries
+- Be compatible with Python 3.11
+- Conform to [PEP 8](https://peps.python.org/pep-0008/)
+- Provide a `--help` option
+- Check for errors in the argument list
+- Check the input files to make sure they conform to the formats exemplified by the sample files
+- Output a list of addresses only if no errors were discovered in the above two steps
+- Write any error messages to stderr
+- Exit with status `0` or `1` to indicate success or failure
 
 > [!WARNING]
 > Study the data carefully: it's not as easy as it looks.
---
+++ challenge.py
@@ -0,0 +1,169 @@
+import argparse
+import json
+import os
+import sys
+import csv
+import xml.etree.ElementTree as ET
+from collections import defaultdict
+
+
+def parse_xml(fp):
+    records = []
+    try:
+        tree = ET.parse(fp)
+        root = tree.getroot()
+    except Exception as e:
+        print(f"Error parsing {fp}\n{e}", file=sys.stderr)
+        sys.exit(1)
+
+    for ent in root.findall(".//ENT"):
+        record = defaultdict()
+        for element in ent:
+            tag = element.tag
+            text = element.text.strip()
+            if text:
+                if tag == "NAME":
+                    record["name"] = text
+                elif tag == "COMPANY":
+                    record["organization"] = text
+                elif tag == "STREET":
+                    record["street"] = text
+                elif tag == "STREET_2":
+                    record["street"] = ", ".join([record["street"], text])
+                elif tag == "STREET_3":
+                    record["street"] = ", ".join([record["street"], text])
+                elif tag == "CITY":
+                    record["city"] = text
+                elif tag == "STATE":
+                    record["state"] = text
+                elif tag == "POSTAL_CODE":
+                    zip = text.replace(" ", "")
+                    if zip[-1] == "-":
+                        zip = zip[:-1]
+                    record["zip"] = zip
+        records.append(record)
+
+    return records
+
+
+def parse_tsv(fp):
+    records = []
+    try:
+        with open(fp, 'r') as f:
+            reader = csv.reader(f, delimiter="\t")
+            next(reader)
+            for row in reader:
+                record = defaultdict()
+                if row[0] == "" and row[3] == "N/A":
+                    row[3] = row[2]
+                    row[2] = ""
+                if row[3] == "N/A":
+                    row[3] = ""
+                if row[1] == "N/M/N":
+                    row[1] = ""
+                if row[0]:
+                    record["name"] = " ".join(filter(None, row[:3]))
+                if row[3]:
+                    record["organization"] = row[3]
+                record["street"] = row[4]
+                record["city"] = row[5]
+                if row[7]:
+                    record["county"] = row[7]
+                record["state"] = row[6]
+                if row[9]:
+                    record["zip"] = "-".join(row[8:])
+                else:
+                    record["zip"] = row[8]
+                records.append(record)
+    except Exception as e:
+        print(f"Error parsing {fp}\n{e}", file=sys.stderr)
+        sys.exit(1)
+
+    return records
+
+
+def parse_txt(fp):
+    records = []
+    try:
+        with open(fp, 'r') as f:
+            entities = f.read().split("\n\n")[1:-1]
+            entities = [i.strip().split("\n  ") for i in entities]
+            for ent in entities:
+                record = defaultdict()
+                record["name"] = ent[0]
+                record["street"] = ent[1]
+                if len(ent) == 4:
+                    location = txt_address_splitter(ent[3])
+                    record["city"] = location[0]
+                    record["county"] = ent[2][:-7]
+                    record["state"] = location[1]
+                    record["zip"] = location[2]
+                else:
+                    location = txt_address_splitter(ent[2])
+                    record["city"] = location[0]
+                    record["state"] = location[1]
+                    record["zip"] = location[2]
+                records.append(record)
+    except Exception as e:
+        print(f"Error parsing {fp}\n{e}", file=sys.stderr)
+        sys.exit(1)
+    return records
+
+
+def txt_address_splitter(text):
+    city, _ = text.split(", ")
+    _ = _.split()
+    zipcode = _[-1]
+    state = " ".join(_[:-1])
+    if zipcode[-1] == "-":
+        zipcode = zipcode[:-1]
+    return [city, state, zipcode]
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="~~~~~BankruptcyWatch file processor~~~~~"
+    )
+    parser.add_argument(
+        "files",
+        nargs="+",
+        type=str,
+        metavar="file",
+        help="file to be processed; accepted formats are .xml, .tsv, .txt",
+    )
+    args = parser.parse_args()
+
+    # check for invalid input files before parsing
+    invalids = []
+    for fp in args.files:
+        _, ext = os.path.splitext(fp)
+        if not os.path.exists(fp):
+            invalids.append([fp, "No such file or directory."])
+        elif ext not in [".xml", ".tsv", ".txt"]:
+            invalids.append(
+                [fp, "Unsupported file extension; use --help for details."])
+
+    if invalids:
+        for i in invalids:
+            print(f"Invalid input file {i[0]}: {i[1]}", file=sys.stderr)
+        sys.exit(1)
+
+    # parse through all files
+    records = []
+    for fp in args.files:
+        _, ext = os.path.splitext(fp)
+        if ext == ".xml":
+            records.extend(parse_xml(fp))
+        elif ext == ".tsv":
+            records.extend(parse_tsv(fp))
+        elif ext == ".txt":
+            records.extend(parse_txt(fp))
+
+    records.sort(key=lambda record: record["zip"])
+    output = json.dumps(records, indent=1)
+    print(output)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
